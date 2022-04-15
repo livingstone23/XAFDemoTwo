51c7c109bb020a2bb4470d83d7f6bca56257650a
livingstone23 <livingstone23@gmail.com>
2022-04-15 15:43:05 +0200

    Initial charge

---
+++ XAFDemoTwo.Module/BusinessObjects/XAFDemoTwoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Data.Entity;
+using System.Data.Common;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.ComponentModel;
+using DevExpress.ExpressApp.EF.Updating;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EF.DesignTime;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+
+namespace XAFDemoTwo.Module.BusinessObjects {
+	public class XAFDemoTwoContextInitializer : DbContextTypesInfoInitializerBase {
+		protected override DbContext CreateDbContext() {
+			DbContextInfo contextInfo = new DbContextInfo(typeof(XAFDemoTwoDbContext), new DbProviderInfo(providerInvariantName: "System.Data.SqlClient", providerManifestToken: "2008"));
+            return contextInfo.CreateInstance();
+		}
+	}
+	[TypesInfoInitializer(typeof(XAFDemoTwoContextInitializer))]
+	public class XAFDemoTwoDbContext : DbContext {
+		public XAFDemoTwoDbContext(String connectionString)
+			: base(connectionString) {
+		}
+		public XAFDemoTwoDbContext(DbConnection connection)
+			: base(connection, false) {
+		}
+		public XAFDemoTwoDbContext() {
+		}
+		public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	}
+}
---
+++ XAFDemoTwo.Web/WebApplication.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.ExpressApp.EF;
+using XAFDemoTwo.Module.BusinessObjects;
+using System.Data.Common;
+using System.Configuration;
+
+namespace XAFDemoTwo.Web {
+    // For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Web.WebApplication
+    public partial class XAFDemoTwoAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private XAFDemoTwo.Module.XAFDemoTwoModule module3;
+        private XAFDemoTwo.Module.Web.XAFDemoTwoAspNetModule module4;
+
+        public XAFDemoTwoAspNetApplication() {
+            InitializeComponent();
+        }
+        protected override IViewUrlManager CreateViewUrlManager() {
+            return new ViewUrlManager();
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionStringXpo"].ConnectionString;
+            if (!String.IsNullOrEmpty(connectionString)){
+			args.ObjectSpaceProviders.Add(new XPObjectSpaceProvider(GetDataStoreProvider(ConfigurationManager.ConnectionStrings["ConnectionStringXpo"].ConnectionString, null), true));
+            }
+            args.ObjectSpaceProviders.Add(new EFObjectSpaceProvider(typeof(XAFDemoTwoDbContext), ConfigurationManager.ConnectionStrings["ConnectionStringEF"].ConnectionString));
+            args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+        }
+        private IXpoDataStoreProvider GetDataStoreProvider(string connectionString, System.Data.IDbConnection connection) {
+            System.Web.HttpApplicationState application = (System.Web.HttpContext.Current != null) ? System.Web.HttpContext.Current.Application : null;
+            IXpoDataStoreProvider dataStoreProvider = null;
+            if(application != null && application["DataStoreProvider"] != null) {
+                dataStoreProvider = application["DataStoreProvider"] as IXpoDataStoreProvider;
+            }
+            else {
+                dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, true);
+                if(application != null) {
+                    application["DataStoreProvider"] = dataStoreProvider;
+                }
+            }
+			return dataStoreProvider;
+        }
+        private void XAFDemoTwoAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+				string message = "The application cannot connect to the specified database, " +
+					"because the database doesn't exist, its version is older " +
+					"than that of the application or its schema does not match " +
+					"the ORM data model structure. To avoid this error, use one " +
+					"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new XAFDemoTwo.Module.XAFDemoTwoModule();
+            this.module4 = new XAFDemoTwo.Module.Web.XAFDemoTwoAspNetModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // XAFDemoTwoAspNetApplication
+            // 
+            this.ApplicationName = "XAFDemoTwo";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.XAFDemoTwoAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
