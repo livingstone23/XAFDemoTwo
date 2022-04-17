5584ee93554258024e448a4e9acf32741b4509bf
livingstone23 <livingstone23@gmail.com>
2022-04-17 12:47:57 +0200

    Access Grid Control Properties (.NET Framework)

---
+++ XAFDemoTwo.Module.Win/Controllers/WinAlternatingRowsController.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace XAFDemoTwo.Module.Win.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class WinAlternatingRowsController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public WinAlternatingRowsController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void WinAlternatingRowsController_ViewControlsCreated(object sender, EventArgs e)
+        {
+
+            //Access Grid Control Properties (.NET Framework)
+            GridListEditor listEditor = ((ListView)View).Editor as GridListEditor;
+            if (listEditor != null)
+            {
+                GridView gridView = listEditor.GridView;
+                gridView.OptionsView.EnableAppearanceOddRow = true;
+                gridView.Appearance.OddRow.BackColor = Color.FromArgb(244, 244, 244);
+            }
+        }
+    }
+}
