717a88cf274ad666427febeff99d69987c6b3f51
livingstone23 <livingstone23@gmail.com>
2022-04-17 13:47:23 +0200

    Make a Property Calculable

---
+++ XAFDemoTwo.Module/BusinessObjects/Payment.cs
@@ -0,0 +1,100 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XAFDemoTwo.Module.BusinessObjects
+{
+    //[DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+
+    [DefaultClassOptions, ImageName("BO_SaleItem")]
+    public class Payment : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Payment(Session session) : base(session) {}
+
+        //Make a Property Calculable
+        private double rate;
+        public double Rate
+        {
+            get
+            {
+                return rate;
+            }
+            set
+            {
+                if (SetPropertyValue(nameof(Rate), ref rate, value))
+                    OnChanged(nameof(Amount));
+            }
+        }
+        private double hours;
+        public double Hours
+        {
+            get
+            {
+                return hours;
+            }
+            set
+            {
+                if (SetPropertyValue(nameof(Hours), ref hours, value))
+                    OnChanged(nameof(Amount));
+            }
+        }
+        [PersistentAlias("Rate * Hours")]
+        public double Amount
+        {
+            get
+            {
+                object tempObject = EvaluateAlias(nameof(Amount));
+                if (tempObject != null)
+                {
+                    return (double)tempObject;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+
+
+
+
+
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        //private string _PersistentProperty;
+        //[XafDisplayName("My display name"), ToolTip("My hint message")]
+        //[ModelDefault("EditMask", "(000)-00"), Index(0), VisibleInListView(false)]
+        //[Persistent("DatabaseColumnName"), RuleRequiredField(DefaultContexts.Save)]
+        //public string PersistentProperty {
+        //    get { return _PersistentProperty; }
+        //    set { SetPropertyValue(nameof(PersistentProperty), ref _PersistentProperty, value); }
+        //}
+
+        //[Action(Caption = "My UI Action", ConfirmationMessage = "Are you sure?", ImageName = "Attention", AutoCommit = true)]
+        //public void ActionMethod() {
+        //    // Trigger a custom business logic for the current record in the UI (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112619.aspx).
+        //    this.PersistentProperty = "Paid";
+        //}
+    }
+}
