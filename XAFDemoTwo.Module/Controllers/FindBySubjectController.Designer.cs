f3d50bfcfcd3b36fa17c1baae39d894b649d87d9
livingstone23 <livingstone23@gmail.com>
2022-04-17 11:17:42 +0200

    Add a Parametrized Action (.NET Framework)

---
+++ XAFDemoTwo.Module/Controllers/FindBySubjectController.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace XAFDemoTwo.Module.Controllers
+{
+    partial class FindBySubjectController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.FindBySubjectAction = new DevExpress.ExpressApp.Actions.ParametrizedAction(this.components);
+            // 
+            // FindBySubjectAction
+            // 
+            this.FindBySubjectAction.Caption = "Find Task by Subject";
+            this.FindBySubjectAction.ConfirmationMessage = null;
+            this.FindBySubjectAction.Id = "FindBySubjectAction ";
+            this.FindBySubjectAction.NullValuePrompt = null;
+            this.FindBySubjectAction.ShortCaption = null;
+            this.FindBySubjectAction.ToolTip = null;
+            this.FindBySubjectAction.TypeOfView = typeof(DevExpress.ExpressApp.View);
+            this.FindBySubjectAction.Execute += new DevExpress.ExpressApp.Actions.ParametrizedActionExecuteEventHandler(this.FindBySubjectAction_Execute);
+            // 
+            // FindBySubjectController
+            // 
+            this.Actions.Add(this.FindBySubjectAction);
+            this.TargetObjectType = typeof(XAFDemoTwo.Module.BusinessObjects.DemoTask);
+            this.TargetViewNesting = DevExpress.ExpressApp.Nesting.Root;
+            this.TargetViewType = DevExpress.ExpressApp.ViewType.ListView;
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.ParametrizedAction FindBySubjectAction;
+    }
+}
---
+++ XAFDemoTwo.Module/Controllers/FindBySubjectController.cs
@@ -0,0 +1,60 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XAFDemoTwo.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class FindBySubjectController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public FindBySubjectController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void FindBySubjectAction_Execute(object sender, ParametrizedActionExecuteEventArgs e)
+        {
+            //Add a Parametrized Action (.NET Framework)
+            IObjectSpace objectSpace = Application.CreateObjectSpace(((ListView)View).ObjectTypeInfo.Type);
+            string paramValue = e.ParameterCurrentValue as string;
+            object obj = objectSpace.FindObject(((ListView)View).ObjectTypeInfo.Type,
+                CriteriaOperator.Parse(string.Format("Contains([Subject], '{0}')", paramValue)));
+            if (obj != null)
+            {
+                DetailView detailView = Application.CreateDetailView(objectSpace, obj);
+                detailView.ViewEditMode = DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+                e.ShowViewParameters.CreatedView = detailView;
+            }
+        }
+    }
+}
