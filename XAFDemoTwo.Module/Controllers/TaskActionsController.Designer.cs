ef4c692666adb6e17f5a5d69261b424f5ac319e9
livingstone23 <livingstone23@gmail.com>
2022-04-17 12:12:03 +0200

    Add an Action with Option Selection (.NET Framework)

---
+++ XAFDemoTwo.Module/Controllers/TaskActionsController.Designer.cs
@@ -0,0 +1,55 @@
+﻿
+namespace XAFDemoTwo.Module.Controllers
+{
+    partial class TaskActionsController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SetTaskAction = new DevExpress.ExpressApp.Actions.SingleChoiceAction(this.components);
+            // 
+            // SetTaskAction
+            // 
+            this.SetTaskAction.Caption = "Set Task";
+            this.SetTaskAction.Category = "Edit";
+            this.SetTaskAction.ConfirmationMessage = null;
+            this.SetTaskAction.Id = "SetTaskAction";
+            this.SetTaskAction.ItemType = DevExpress.ExpressApp.Actions.SingleChoiceActionItemType.ItemIsOperation;
+            this.SetTaskAction.ToolTip = null;
+            // 
+            // TaskActionsController
+            // 
+            this.Actions.Add(this.SetTaskAction);
+            this.TargetObjectType = typeof(XAFDemoTwo.Module.BusinessObjects.DemoTask);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SingleChoiceAction SetTaskAction;
+    }
+}
---
+++ XAFDemoTwo.Module/Controllers/TaskActionsController.cs
@@ -0,0 +1,124 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XAFDemoTwo.Module.BusinessObjects;
+
+namespace XAFDemoTwo.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class TaskActionsController : ViewController
+    {
+        //Add an Action with Option Selection (.NET Framework) - 1.1
+        private ChoiceActionItem setPriorityItem;
+        private ChoiceActionItem setStatusItem;
+        public TaskActionsController() {
+            InitializeComponent();
+            setPriorityItem =
+            new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(DemoTask), "Priority"), null);
+            SetTaskAction.Items.Add(setPriorityItem);
+            FillItemWithEnumValues(setPriorityItem, typeof(Priority));
+            setStatusItem =
+            new ChoiceActionItem(CaptionHelper.GetMemberCaption(typeof(DemoTask), "Status"), null);
+            SetTaskAction.Items.Add(setStatusItem);
+            FillItemWithEnumValues(setStatusItem, typeof(TaskStatus));
+
+            //Add an Action with Option Selection (.NET Framework) - 1.2
+            SetTaskAction.Execute += SetTaskAction_Execute;
+        }
+        private void FillItemWithEnumValues(ChoiceActionItem parentItem, Type enumType) {
+            foreach(object current in Enum.GetValues(enumType)) {
+                EnumDescriptor ed = new EnumDescriptor(enumType);
+                ChoiceActionItem item = new ChoiceActionItem(ed.GetCaption(current), current);
+                item.ImageName = ImageLoader.Instance.GetEnumValueImageName(current);
+                parentItem.Items.Add(item);
+            }
+        }
+
+
+        //Add an Action with Option Selection (.NET Framework) - 1.3
+        private void SetTaskAction_Execute(object sender, SingleChoiceActionExecuteEventArgs e)
+        {
+            IObjectSpace objectSpace = View is ListView ?  Application.CreateObjectSpace(typeof(DemoTask)) : View.ObjectSpace;
+            ArrayList objectsToProcess = new ArrayList(e.SelectedObjects);
+            if (e.SelectedChoiceActionItem.ParentItem == setPriorityItem)
+            {
+                foreach (Object obj in objectsToProcess)
+                {
+                    DemoTask objInNewObjectSpace = (DemoTask)objectSpace.GetObject(obj);
+                    objInNewObjectSpace.Priority = (BusinessObjects.Priority)(Priority)e.SelectedChoiceActionItem.Data;
+                }
+            }
+            else
+                if (e.SelectedChoiceActionItem.ParentItem == setStatusItem)
+            {
+                foreach (Object obj in objectsToProcess)
+                {
+                    DemoTask objInNewObjectSpace = (DemoTask)objectSpace.GetObject(obj);
+                    objInNewObjectSpace.Status = (DevExpress.Persistent.Base.General.TaskStatus)(TaskStatus)e.SelectedChoiceActionItem.Data;
+                }
+            }
+            if (View is DetailView && ((DetailView)View).ViewEditMode == ViewEditMode.View)
+            {
+                objectSpace.CommitChanges();
+            }
+            if (View is ListView)
+            {
+                objectSpace.CommitChanges();
+                View.ObjectSpace.Refresh();
+            }
+        }
+
+
+        //// Use CodeRush to create Controllers and Actions with a few keystrokes.
+        //// https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        //public TaskActionsController()
+        //{
+        //    InitializeComponent();
+        //    // Target required Views (via the TargetXXX properties) and create their Actions.
+        //}
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+
+
+
+    public enum Priority
+    {
+        [ImageName("State_Priority_Low")]
+        Low = 0,
+        [ImageName("State_Priority_Normal")]
+        Normal = 1,
+        [ImageName("State_Priority_High")]
+        High = 2
+    }
+
+
+
+}
