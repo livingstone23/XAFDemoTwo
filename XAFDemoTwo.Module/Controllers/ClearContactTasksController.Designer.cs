199ae4990f1f8ddd3cc9ac2076c915c674bd07a5
livingstone23 <livingstone23@gmail.com>
2022-04-17 11:01:02 +0200

    add a simple action

---
+++ XAFDemoTwo.Module/Controllers/ClearContactTasksController.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace XAFDemoTwo.Module.Controllers
+{
+    partial class ClearContactTasksController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClearTaskAction = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // ClearTaskAction
+            // 
+            this.ClearTaskAction.Caption = "Clear Tasks";
+            this.ClearTaskAction.Category = "View";
+            this.ClearTaskAction.ConfirmationMessage = "Are you sure you want to clear the Tasks list?";
+            this.ClearTaskAction.Id = "ClearTasksAction";
+            this.ClearTaskAction.ImageName = "Action_Clear";
+            this.ClearTaskAction.ToolTip = null;
+            this.ClearTaskAction.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.ClearTaskAction_Execute);
+            // 
+            // ClearContactTasksController
+            // 
+            this.Actions.Add(this.ClearTaskAction);
+            this.TargetObjectType = typeof(XAFDemoTwo.Module.BusinessObjects.Contact);
+            this.TargetViewType = DevExpress.ExpressApp.ViewType.DetailView;
+            this.Activated += new System.EventHandler(this.ClearContactTasksController_Activated);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction ClearTaskAction;
+    }
+}
---
+++ XAFDemoTwo.Module/Controllers/ClearContactTasksController.cs
@@ -0,0 +1,75 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XAFDemoTwo.Module.BusinessObjects;
+
+namespace XAFDemoTwo.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class ClearContactTasksController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public ClearContactTasksController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void ClearTaskAction_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            //
+            while (((Contact)View.CurrentObject).Tasks.Count > 0)
+            {
+                ((Contact)View.CurrentObject).Tasks.Remove(((Contact)View.CurrentObject).Tasks[0]);
+            }
+            ObjectSpace.SetModified(View.CurrentObject);
+        }
+
+        private void ClearContactTasksController_Activated(object sender, EventArgs e)
+        {
+            // Enables the ClearTasks Action if the current Detail View's ViewEditMode property
+            // is set to ViewEditMode.Edit.
+            //ClearTasksAction.Enabled.SetItemValue("EditMode", ((DetailView)View).ViewEditMode == ViewEditMode.Edit);
+            //((DetailView)View).ViewEditModeChanged += new EventHandler<EventArgs>(ClearContactTasksController_ViewEditModeChanged);
+
+
+
+        }
+
+        // Manages the ClearTasks Action enabled state.
+        void ClearContactTasksController_ViewEditModeChanged(object sender, EventArgs e)
+        {
+            //ClearTasksAction.Enabled.SetItemValue("EditMode",((DetailView)View).ViewEditMode == ViewEditMode.Edit);
+        }
+
+
+    }
+}
